3244ccced7588c272efa91e23a195825537ada22
ImRkofficial <rkbagra0490@gmail.com>
2025-05-20 11:43:02 +0530

    fix the gap and font issues in the hero banner

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -9,32 +9,29 @@ const Hero = () => {
         className="  fixed top-0 left-0 inset-0 bg-[url('/images/hero-image.png')] bg-cover bg-center opacity-30 -z-10"
         aria-hidden="true"
       ></div>
-      {/* Content */}
-      <div className="relative max-w-[776px] mx-auto flex justify-center items-center py-12 md:py-[102px]  w-full z-10">
-        {/* Overlay from .elementor-background-overlay */}
-        <div className="px-2 md:px-2 items-center flex flex-col gap-6 text-white w-full">
-          <h1 className="text-2xl text-white! sm:text-3xl md:text-5xl text-center leading-tight md:leading-[67px] lg:text-5xl font-bold  ">
-            Employee Monitoring and 
-            Productivity Tracking Solution
+      <div className="relative max-w-[785px] mx-auto flex justify-center items-center py-[70px] md:py-[102px]  w-full z-10">
+        <div className="px-2 md:px-2 items-center flex flex-col gap-5 md:gap-6 text-white w-full">
+          <h1 className=" text-white! text-center">
+            Employee Monitoring and Productivity Tracking Solution
           </h1>
-          <div className="flex flex-col md:flex-row items-center w-full --font-roboto justify-between md:text-xl font-bold  gap-5 md:gap-0">
-            <div className="flex flex-col gap-2 items-start justify-center w-full md:w-auto">
-              <span className="text-[15px] md:text-[16px] flex items-center ">
+          <div className="flex flex-col md:flex-row items-center w-full --font-roboto justify-between  md:gap-5 gap-2">
+            <div className="flex flex-col gap-2 items-center md:items-start justify-center w-full md:w-auto">
+              <span className="text-[16px] flex items-center font-[600]">
                 <Check className="mr-2 bg-[#24C16F] text-white rounded-full  h-5 w-5" />
                 Monitor employees in real time{" "}
               </span>
-              <span className="text-[15px] md:text-[16px] flex items-center ">
+              <span className="text-[16px] flex items-center font-[600] ">
                 <Check className="mr-2 bg-[#24C16F] text-white rounded-full  h-5 w-5" />
                 Productivity and behavior analysis
               </span>
             </div>
-            <div className="flex flex-col gap-2 items-start justify-center w-full md:w-auto">
-              <span className="text-[15px] md:text-[16px] flex items-center ">
+            <div className="flex flex-col gap-2 items-center md:items-start justify-center w-full md:w-auto">
+              <span className="text-[16px] flex items-center font-[600]">
                 <Check className="mr-2 bg-[#24C16F] text-white rounded-full  h-5 w-5" />
                 Data Protection and Peace of Mind
               </span>
 
-              <span className="text-[15px] md:text-[16px] flex items-center ">
+              <span className="flex items-center font-[600]">
                 <Check className="mr-2 bg-[#24C16F] text-white rounded-full  h-5 w-5" />
                 Motivates employees to further perform better{" "}
               </span>
