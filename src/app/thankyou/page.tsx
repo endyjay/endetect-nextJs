2731922c649af807a49386a657174e31b71ca8da
ImRkofficial <rkbagra0490@gmail.com>
2025-05-21 13:36:52 +0530

    Update button link for features page

--- src/app/thankyou/page.tsx
+++ src/app/thankyou/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
         <div>
             <h2 className='text-white! mb-[20px] text-center text-[25px] md:text-[42px] font-[600]!'>Thanks For contacting us we will get back to you soon!!</h2>
             <div className='w-full text-center'>
-            <Link href={"/features"} className='text-center inline-flex mx-auto justify-center bg-[#5cb85c] py-[17px] px-[30px]'>
+            <Link href={"/best-pc-monitoring-software"} className='text-center inline-flex mx-auto justify-center bg-[#5cb85c] py-[17px] px-[30px]'>
             <span className='text-[15px] leading-[1] text-white'>BACK TO SITE</span>
             </Link>
             </div>
