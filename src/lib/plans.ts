5886137a6881ef5fe1f0808ead1f03f3da7eee27
batmanCTA9 <rahul.dudi@cta9.com>
2025-05-22 17:07:07 +0530

    update pricing page

--- src/lib/plans.ts
+++ src/lib/plans.ts
@@ -20,30 +20,28 @@ export const plans = [
       true, // PC Info (Hardware, OS)
       true, // Free Future Updates
       true, // USB Drive Access Logs
+      true, // Team Productivity Analysis
+      true, // On Demand Screenshot
       false, // Website Visit History
       false, // Social Media Activity Report
       false, // Video Access Report
-      true, // Team Productivity Analysis
-      true, // On Demand Screenshot
+
       false, // Create Supervisors / Managers
       false, // Keyboard Activity Monitor
       false, // Automated Triggers and Alerts
       false, // Track File Copy / Delete / Rename
       false, // Track Text Copy Operations
       false, // Advance Search
-      // true, // Block USB Drive
-      // true, // Block Task Manager
+
       false, // Abusive Behavior Detection
       false, // Malicious Behavior Detection
-      // false, // Real Time Live View
-      false, // Special Key Capture Report
-      // false, // Merge Users
-      "6 Months", // Data Retention
       false, // Data Storage on your Cloud
+      false, // Special Key Capture Report
       false, // Customization
       false, // Download Backup
       false, // White Label
       false, // Multiple Admins
+      "6 Months", // Data Retention
       "Email", // Support
     ],
   },
@@ -68,30 +66,28 @@ export const plans = [
       true, // PC Info (Hardware, OS)
       true, // Free Future Updates
       true, // USB Drive Access Logs
+      true, // Team Productivity Analysis
+      true, // On Demand Screenshot
       true, // Website Visit History
       true, // Social Media Activity Report
       true, // Video Access Report
-      true, // Team Productivity Analysis
-      true, // On Demand Screenshot
+
       true, // Create Supervisors / Managers
       true, // Keyboard Activity Monitor
       "Partial", // Automated Triggers and Alerts
       true, // Track File Copy / Delete / Rename
       true, // Track Text Copy Operations
       true, // Advance Search
-      // true, // Block USB Drive
-      // true, // Block Task Manager
+
       true, // Abusive Behavior Detection
       true, // Malicious Behavior Detection
-      // false, // Real Time Live View
-      false, // Special Key Capture Report
-      // false, // Merge Users
-      "1 Year", // Data Retention
       "Optional*", // Data Storage on your Cloud
+      false, // Special Key Capture Report
       false, // Customization
       false, // Download Backup
       false, // White Label
       false, // Multiple Admins
+      "1 Year", // Data Retention
       "Email, Phone", // Support
     ],
   },
@@ -116,30 +112,28 @@ export const plans = [
       true, // PC Info (Hardware, OS)
       true, // Free Future Updates
       true, // USB Drive Access Logs
+      true, // Team Productivity Analysis
+      true, // On Demand Screenshot
       true, // Website Visit History
       true, // Social Media Activity Report
       true, // Video Access Report
-      true, // Team Productivity Analysis
-      true, // On Demand Screenshot
+
       true, // Create Supervisors / Managers
       true, // Keyboard Activity Monitor
       "Full", // Automated Triggers and Alerts
       true, // Track File Copy / Delete / Rename
       true, // Track Text Copy Operations
       true, // Advance Search
-      // true, // Block USB Drive
-      // true, // Block Task Manager
+
       true, // Abusive Behavior Detection
       true, // Malicious Behavior Detection
-      // true, // Real Time Live View
-      true, // Special Key Capture Report
-      // true, // Merge Users
-      "Unlimited", // Data Retention
       true, // Data Storage on your Cloud
+      true, // Special Key Capture Report
       true, // Customization
       true, // Download Backup
       true, // White Label
       true, // Multiple Admins
+      "Unlimited", // Data Retention
       "Account Manager", // Support
     ],
   },
@@ -157,29 +151,29 @@ export const featureList = [
   "PC Info (Hardware, OS)",
   "Free Future Updates",
   "USB Drive Access Logs",
+  "Team Productivity Analysis",
+  "On Demand Screenshot",
   "Website Visit History",
   "Social Media Activity Report",
   "Video Access Report",
-  "Team Productivity Analysis",
-  "On Demand Screenshot",
+
   "Create Supervisors / Managers",
   "Keyboard Activity Monitor",
   "Automated Triggers and Alerts",
   "Track File Copy / Delete / Rename",
   "Track Text Copy Operations",
   "Advance Search",
-  // "Block USB Drive",
-  // "Block Task Manager",
+
   "Abusive Behavior Detection",
   "Malicious Behavior Detection",
-  // "Real Time Live View",
-  "Special Key Capture Report",
-  // "Merge Users",
-  "Data Retention",
+
   "Data Storage on your Cloud",
+  "Special Key Capture Report",
+
   "Customization",
   "Download Backup",
   "White Label",
   "Multiple Admins",
+  "Data Retention",
   "Support",
 ];
